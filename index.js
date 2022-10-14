a2d51b7dd2a8ac88b92f5330cb382cafedd6ea96
KakaniEmpires10 <muhammadalimkakani@gmail.com>
2022-10-14 12:44:23 +0700

    File Drum Web With javaScript

---
+++ index.js
@@ -0,0 +1,75 @@
+var numberofDrum = document.querySelectorAll(".drum").length;
+
+for (i = 0; i < numberofDrum; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", buttonClick);
+
+}
+
+
+function play (){
+    var audio = new Audio ("sounds/tom-1.mp3");
+    audio.play();
+}
+
+function colorChange() {
+    this.style.color = "#fff";
+}
+
+function buttonClick(){
+    var buttonInnerHTML = this.innerHTML;
+    keySound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+    
+}
+
+document.addEventListener("keydown", makeSound);
+
+function makeSound(event){
+    keySound(event.key);
+    buttonAnimation(event.key);
+
+}
+
+function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+    setTimeout(function(){activeButton.classList.remove("pressed");}, 100);
+}
+
+function keySound(key){
+
+    switch (key) {
+        case "w":
+            var tom1 = new Audio ("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio ("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "s":
+            var tom3 = new Audio ("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "d":
+            var tom4 = new Audio ("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            var crash = new Audio ("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            var kick = new Audio ("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            var snare = new Audio ("sounds/snare.mp3");
+            snare.play();
+            break;
+    
+        default: console.log("You Did Somethings Wrong Dude !");
+            break;
+    }
+}
